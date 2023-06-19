e7a90d89ac8f2652d3074bfcdbeed07f302c595e
Hakan Lindestaf <hakan@lindestaf.com>
2023-06-19 13:24:18 -0500

    Added files to show issue with Cancel NavigateViewForResultAsync in https://github.com/unoplatform/uno.extensions/issues/1480

--- UnoApp5/App.cs
+++ UnoApp5/App.cs
@@ -62,6 +62,7 @@ namespace UnoApp5
             views.Register(
                 new ViewMap(ViewModel: typeof(ShellViewModel)),
                 new ViewMap<MainPage, MainViewModel>(),
+                new ViewMap<OverlayPage>(),
                 new DataViewMap<SecondPage, SecondViewModel, Entity>()
             );
 
@@ -71,6 +72,7 @@ namespace UnoApp5
                     {
                     new RouteMap("Main", View: views.FindByViewModel<MainViewModel>()),
                     new RouteMap("Second", View: views.FindByViewModel<SecondViewModel>()),
+                    new RouteMap("Overlay", View: views.FindByView<OverlayPage>()),
                     }
                 )
             );
--- UnoApp5/Presentation/MainViewModel.cs
+++ UnoApp5/Presentation/MainViewModel.cs
@@ -13,10 +13,13 @@ namespace UnoApp5.Presentation
             _navigator = navigator;
             Title = "Main";
             GoToSecond = new AsyncRelayCommand(GoToSecondView);
+            DisplayOverlay = new AsyncRelayCommand(DisplayOverlayView);
         }
         public string? Title { get; }
 
         public ICommand GoToSecond { get; }
+
+        public ICommand DisplayOverlay { get; }
 
 
         private async Task GoToSecondView()
@@ -24,5 +27,11 @@ namespace UnoApp5.Presentation
             await _navigator.NavigateViewModelAsync<SecondViewModel>(this, data: new Entity(Name!));
         }
 
+        private async Task DisplayOverlayView()
+        {
+            var popupCancellationTokenSource = new CancellationTokenSource(2_000);
+
+            var result = await _navigator.NavigateViewForResultAsync<OverlayPage, bool>(this, qualifier: Qualifiers.Dialog, cancellation: popupCancellationTokenSource.Token);
+        }
     }
 }
